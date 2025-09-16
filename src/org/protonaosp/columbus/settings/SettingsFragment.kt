220bd1dea970c1bb6298649f007a8a963984ce91
Ghosuto <clash.raja10@gmail.com>
2025-09-16 16:19:19 +0000

    Fixup https://github.com/Lunaris-AOSP/packages_apps_ColumbusService/commit/826c9cec315ff41ffdd4589f41ab984c877fa77b
    - Add back DerpFest copyright headers
    
    - Thnx to crDroid
    
    Change-Id: I9cb3890dc4b43fd64c5592cc9379ef3132e357d1
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- src/org/protonaosp/columbus/settings/SettingsFragment.kt
+++ src/org/protonaosp/columbus/settings/SettingsFragment.kt
@@ -1,6 +1,7 @@
 /*
  * SPDX-FileCopyrightText: The Proton AOSP Project
  * SPDX-FileCopyrightText: TheParasiteProject
+ * SPDX-FileCopyrightText: DerpFest AOSP
  * SPDX-FileCopyrightText: crDroid Android Project
  * SPDX-License-Identifier: GPL-3.0
  */
